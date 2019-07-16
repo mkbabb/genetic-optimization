c38d6576a6d8c94ec664349bf2b18e40e2614ac3
Michael Babb <mike7400@gmail.com>
2019-07-15 23:11:32 -0400

    Minor changes

--- erate-genetic.py
+++ erate-genetic.py
@@ -1,14 +1,12 @@
+import math
 import random
 import timeit
+from dataclasses import dataclass
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
 from scipy.signal import fftconvolve
-import math
-
-from dataclasses import dataclass
-
 
 SEED = 0xDEADBEEF
 
@@ -40,13 +38,13 @@ class Critter(object):
         return f"{self.fitness}"
 
 
-def life(data: pd.DataFrame,
-         critter: Critter,
-         buckets: Dict[int, Dict[str, float]],
-         bucket_count: int,
-         max_bucket: int,
-         full_buckets: List[bool]):
+def calc_erate_stats(data: pd.DataFrame,
+                     critter: Critter,
+                     buckets: Dict[int, Dict[str, float]],
+                     max_bucket: int,
+                     full_buckets: List[bool]) -> None:
     N = len(data)
+    bucket_count = len(buckets)
     for j in range(N):
         k = critter.genes[j][1]\
             if critter.genes[j][0]\
@@ -84,16 +82,18 @@ def life(data: pd.DataFrame,
     critter.fitness = float(t)
 
 
-def and_critter(critter1, critter2):
+def and_critter(critter1: Critter,
+                critter2: Critter) -> None:
     for i in range(len(critter1.genes)):
         critter1.genes[i][0] &= critter2.genes[i][0]
 
 
-def mate(N: int,
-         critters: List[Critter],
-         max_fitness: float,
-         mutation_count: int,
-         parent_count: int = 2):
+def mate_critters(data: pd.DataFrame,
+                  critters: List[Critter],
+                  max_fitness: float,
+                  mutation_count: int,
+                  parent_count: int = 2) -> List[Critter]:
+    N = len(data)
     L = len(critters)
 
     ratio: float = 0
@@ -116,7 +116,6 @@ def mate(N: int,
             k += 1
 
     children = critters[:parent_count]
-
     for i in range(L - parent_count):
         child = Critter(N)
 
@@ -137,24 +136,24 @@ def mate(N: int,
     return children
 
 
-def optimize_t(data: pd.DataFrame,
-               bucket_count: int,
-               max_bucket: int,
-               population_size: int = 10,
-               mutation_percentage: float = 0.1,
-               parent_count: int = 2,
-               iterations: int = 1000) -> List[Dict[str, Union[int, float]]]:
+def optimize_buckets(data: pd.DataFrame,
+                     bucket_count: int,
+                     max_bucket: int,
+                     population_size: int = 10,
+                     mutation_rate: float = 0.1,
+                     parent_count: int = 2,
+                     iterations: int = 1000) -> List[Dict[str, Union[int, float]]]:
     N = len(data)
     max_bucket = max(max_bucket, N // bucket_count)
-    buckets = {i: {"average_discount": 0,
-                   "total_discount": 0,
-                   "total_cost": 0,
-                   "discount_cost": 0,
-                   "count": 0} for i in range(bucket_count)}
-
+    buckets: Dict[int, Dict[str, float]] = {i: {"average_discount": 0,
+                                                "total_discount": 0,
+                                                "total_cost": 0,
+                                                "discount_cost": 0,
+                                                "count": 0} for i in range(bucket_count)}
     full_buckets = [False for i in range(bucket_count)]
+
     extremal_fitness: float = 0.1
-    mutation_count = int(math.ceil(N * mutation_percentage))
+    mutation_count = int(math.ceil(N * mutation_rate))
 
     critters = [Critter(N) for i in range(population_size)]
 
@@ -162,12 +161,11 @@ def optimize_t(data: pd.DataFrame,
         for j in range(population_size):
             critter = critters[j]
 
-            life(data,
-                 critter,
-                 buckets,
-                 bucket_count,
-                 max_bucket,
-                 full_buckets)
+            calc_erate_stats(data,
+                             critter,
+                             buckets,
+                             max_bucket,
+                             full_buckets)
 
             if (critter.fitness > extremal_fitness):
                 extremal_fitness = critter.fitness
@@ -178,14 +176,20 @@ def optimize_t(data: pd.DataFrame,
                         f"lea: {data.loc[i, 'lea-number']}, bucket: {critter.genes[i][1]}")
                     critter.genes[i][0] = 1
 
-        critters = mate(N,
-                        critters,
-                        extremal_fitness,
-                        mutation_count,
-                        parent_count)
+        critters = mate_critters(data,
+                                 critters,
+                                 extremal_fitness,
+                                 mutation_count,
+                                 parent_count)
 
 
 data = pd.read_csv("erate-data.csv",
                    names=["lea-number", "discount", "cost"])[:30]
 
-optimize_t(data, 3, 2, 10, 0.3, 2, 500)
+optimize_buckets(data,
+                 bucket_count=3,
+                 max_bucket=2,
+                 population_size=10,
+                 mutation_rate=0.3,
+                 parent_count=2,
+                 iterations=1000)
